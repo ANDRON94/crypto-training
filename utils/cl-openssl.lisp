242f820ede6b96859e6371da54d74b67d77e33a0
Andrii Tymchuk <Andrii.Tymchuk@harmonicinc.com>
2017-03-14 17:29:01 +0200

    [Update] Start to port OpenSSL functions:
    - specify types of pointers(only for doc purposes);
    - change Lisp interface(alpha version).

--- utils/cl-openssl.lisp
+++ utils/cl-openssl.lisp
@@ -16,38 +16,51 @@
 ;;; define types
 
 (cffi:defcstruct aes-key
-  (rd-key :pointer)
+  (rd-key (:pointer :unsigned-int))
   (rounds :int))
 
 ;;; define functions
 
 (cffi:defcfun ("AES_set_encrypt_key" ffi-aes-set-encrypt-key) :int
-  (user-key :pointer)
+  (user-key (:pointer :unsigned-char))
   (bits-count :int)
-  (key :pointer))
+  (key (:pointer :aes-key)))
 
 (cffi:defcfun ("AES_set_decrypt_key" ffi-aes-set-decrypt-key) :int
-  (user-key :pointer)
+  (user-key (:pointer :unsigned-char))
   (bits-count :int)
-  (key :pointer))
+  (key (:pointer :aes-key)))
 
 (cffi:defcfun ("AES_encrypt" ffi-aes-encrypt) :void
-  (in :pointer)
-  (out :pointer)
-  (key :pointer))
+  (in (:pointer :unsigned-char))
+  (out (:pointer :unsigned-char))
+  (key (:pointer :aes-key)))
 
 (cffi:defcfun ("AES_decrypt" ffi-aes-decrypt) :void
-  (in :pointer)
-  (out :pointer)
-  (key :pointer))
+  (in (:pointer :unsigned-char))
+  (out (:pointer :unsigned-char))
+  (key (:pointer :aes-key)))
 
 ;;;; INTERFACE
 
-(defun make-aes-encryption-key (user-key bits-count) ; Maybe remove 'bits-count' parameter!
+(defparameter +AES-KEY-SIZES+ '(16 24 32)
+  "List of all possible AES key sizes in octets(bytes) count.")
+
+(defparameter +AES-BLOCK-SIZE+ 16
+  "AES block size in octets(bytes) count")
+
+(defun make-aes-encryption-key (user-key)
+  "Create AES encryption key using 'key schedule'
+procedure based on USER-KEY.
+USER-KEY is a vector of bytes"
   ;; TODO!
   nil)
 
-(defun make-aes-decryption-key (user-key bits-count) ; Maybe remove 'bits-count' parameter!
+(defun make-aes-decryption-key (user-key)
+  ;; TODO!
+  nil)
+
+(defun release-aes-key (key)
   ;; TODO!
   nil)
 
