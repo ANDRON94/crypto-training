aac4e22f6d5bd28a19021768b8a6a445b4829c6d
Andrii Tymchuk <Andrii.Tymchuk@harmonicinc.com>
2017-03-17 17:27:16 +0200

    [Update] Start to implement encryption/decryption using AES.

--- lesson2/solution.lisp
+++ lesson2/solution.lisp
@@ -5,5 +5,27 @@
 
 ;;; INTERFACE
 
+(defgeneric aes-encrypt (input mode action)
+  (:documentation "Make encryption/decryption of INPUT byte vector
+using a AES block cipher.
+MODE specify in which mode encryption will be done:
+1) cipher block chain(:cbc);
+2) random counter(:ctr).
+ACTION specify which process will be executed:
+1) encryption(:encrypt);
+2) decryption(:decrypt)."))
+
 ;;; IMPLEMENTATION
+
+(defmethod aes-encrypt (input (mode (eql :cbc)) (action (eql :encrypt)))
+  :cbc-encrypt)
+
+(defmethod aes-encrypt (input (mode (eql :cbc)) (action (eql :decrypt)))
+  :cbc-decrypt)
+
+(defmethod aes-encrypt (input (mode (eql :ctr)) (action (eql :encrypt)))
+  :ctr-encrypt)
+
+(defmethod aes-encrypt (input (mode (eql :ctr)) (action (eql :decrypt)))
+  :ctr-decrypt)
 
